20f35f89814f586bc5cfe48ea0a571a6e19b6f98
KenNgo <phucuongngo@gmail.com>
2020-02-08 17:02:14 -0600

    fix bugs

--- src/script.sh
+++ src/script.sh
@@ -2,20 +2,20 @@
 
  javac -d bin arc180006/*.java 
  cd bin
- java arc180006.MergeSort 1600 1 >> ../output.txt 
- java arc180006.MergeSort 1600 2 >> ../output.txt 
- java arc180006.MergeSort 1600 3 >> ../output.txt 
- java arc180006.MergeSort 1600 4 >> ../output.txt 
- java arc180006.MergeSort 3200 1 >> ../output.txt 
- java arc180006.MergeSort 3200 2 >> ../output.txt 
- java arc180006.MergeSort 3200 3 >> ../output.txt 
- java arc180006.MergeSort 3200 4 >> ../output.txt 
- java arc180006.MergeSort 6400 1 >> ../output.txt 
- java arc180006.MergeSort 6400 2 >> ../output.txt 
- java arc180006.MergeSort 6400 3 >> ../output.txt 
- java arc180006.MergeSort 6400 4 >> ../output.txt 
- java arc180006.MergeSort 12800 1 >> ../output.txt 
- java arc180006.MergeSort 12800 2 >> ../output.txt 
- java arc180006.MergeSort 12800 3 >> ../output.txt 
- java arc180006.MergeSort 12800 4 >> ../output.txt 
+ java arc180006.MergeSort 16000000 1 >> ../output.txt 
+ java arc180006.MergeSort 16000000 2 >> ../output.txt 
+ java arc180006.MergeSort 16000000 3 >> ../output.txt 
+ java arc180006.MergeSort 16000000 4 >> ../output.txt 
+ java arc180006.MergeSort 32000000 1 >> ../output.txt 
+ java arc180006.MergeSort 32000000 2 >> ../output.txt 
+ java arc180006.MergeSort 32000000 3 >> ../output.txt 
+ java arc180006.MergeSort 32000000 4 >> ../output.txt 
+ java arc180006.MergeSort 64000000 1 >> ../output.txt 
+ java arc180006.MergeSort 64000000 2 >> ../output.txt 
+ java arc180006.MergeSort 64000000 3 >> ../output.txt 
+ java arc180006.MergeSort 64000000 4 >> ../output.txt 
+ java arc180006.MergeSort 128000000 1 >> ../output.txt 
+ java arc180006.MergeSort 128000000 2 >> ../output.txt 
+ java arc180006.MergeSort 128000000 3 >> ../output.txt 
+ java arc180006.MergeSort 128000000 4 >> ../output.txt 
  
